9844381043e31992abfd942769c0870fbb85ab7c
Ib Havn <iha@via.dk>
2020-03-15 13:19:43 +0100

    Doc update

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,16 +1,15 @@
 var searchData=
 [
-  ['list',['list',['../structlist.html',1,'']]],
   ['list_2eh',['list.h',['../list_8h.html',1,'']]],
-  ['list_5fadditem',['list_addItem',['../list_8h.html#a7763817d7a1973f7d44d10d2d596fb0e',1,'list.c']]],
-  ['list_5fcreate',['list_create',['../list_8h.html#a055723090302244b9a438e7705c2f7b3',1,'list.c']]],
+  ['list_5fadditem',['list_addItem',['../list_8h.html#a7763817d7a1973f7d44d10d2d596fb0e',1,'list.h']]],
+  ['list_5fcreate',['list_create',['../list_8h.html#a055723090302244b9a438e7705c2f7b3',1,'list.h']]],
   ['list_5fempty',['LIST_EMPTY',['../list_8h.html#a1586779599c807b2e3484e9ce746e9c7a89dc90b0b4aa46712b045d6ca7a887d3',1,'list.h']]],
   ['list_5ferror',['LIST_ERROR',['../list_8h.html#a1586779599c807b2e3484e9ce746e9c7a67c1787aedb6c22095832b9d88e0675e',1,'list.h']]],
-  ['list_5fgetitem',['list_getItem',['../list_8h.html#a61ef09343055f2face210cf69f949736',1,'list.c']]],
-  ['list_5fnoofitems',['list_noOfItems',['../list_8h.html#a49cedd467302b7b80d3bc1a6eab1f167',1,'list.c']]],
+  ['list_5fgetitem',['list_getItem',['../list_8h.html#a61ef09343055f2face210cf69f949736',1,'list.h']]],
+  ['list_5fnoofitems',['list_noOfItems',['../list_8h.html#a49cedd467302b7b80d3bc1a6eab1f167',1,'list.h']]],
   ['list_5fnot_5ffound',['LIST_NOT_FOUND',['../list_8h.html#a1586779599c807b2e3484e9ce746e9c7a9727bf70981c8a7acc7e268b82bde162',1,'list.h']]],
   ['list_5fnull',['LIST_NULL',['../list_8h.html#a1586779599c807b2e3484e9ce746e9c7a0be1232eaa67e7440aa9838dd87387d8',1,'list.h']]],
   ['list_5fok',['LIST_OK',['../list_8h.html#a1586779599c807b2e3484e9ce746e9c7a918f6a1af240ad9b362d03c58ceff6dc',1,'list.h']]],
-  ['list_5fremoveitem',['list_removeItem',['../list_8h.html#a7a1336e5d4c741fa05b4c0b7fa9fed07',1,'list.c']]],
+  ['list_5fremoveitem',['list_removeItem',['../list_8h.html#a7a1336e5d4c741fa05b4c0b7fa9fed07',1,'list.h']]],
   ['list_5freturncode_5ft',['list_ReturnCode_t',['../list_8h.html#a1586779599c807b2e3484e9ce746e9c7',1,'list.h']]]
 ];
--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,33 +1,30 @@
 var indexSectionsWithContent =
 {
-  0: "lnr",
-  1: "ln",
+  0: "lr",
+  1: "l",
   2: "l",
   3: "l",
   4: "l",
-  5: "l",
-  6: "r"
+  5: "r"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "enums",
-  5: "enumvalues",
-  6: "pages"
+  1: "files",
+  2: "functions",
+  3: "enums",
+  4: "enumvalues",
+  5: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Data Structures",
-  2: "Files",
-  3: "Functions",
-  4: "Enumerations",
-  5: "Enumerator",
-  6: "Pages"
+  1: "Files",
+  2: "Functions",
+  3: "Enumerations",
+  4: "Enumerator",
+  5: "Pages"
 };
 
